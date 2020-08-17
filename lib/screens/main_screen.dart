2a11532be94ceee949b2f09847cc80a6d31ae317
volkannaktass <volkanaktas98@gmail.com>
2020-08-17 17:15:14 +0300

    made categories and product with json-server

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http_demo/data/data.api/category_api.dart';
+import 'package:http_demo/models/category.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MainScreenState();
+  }
+}
+
+class MainScreenState extends State {
+  List<Category> categories = List<Category>();
+  List<Widget> categoryWidgets = List<Widget>();
+
+  @override
+  void initState() {
+    getCategoriesFromApi();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Alışveriş Sistemi",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: categoryWidgets,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void getCategoriesFromApi() {
+    CategoryApi.getCategories().then((response) {
+      setState(() {
+        Iterable list = json.decode(response.body);
+        this.categories =
+            list.map((category) => Category.fromJson(category)).toList();
+        getCategoryWidgets();
+      });
+    });
+  }
+
+  List<Widget> getCategoryWidgets() {
+    for (int i = 0; i < categories.length; i++) {
+      categoryWidgets.add(getCategoryWidget(categories[i]));
+    }
+    return categoryWidgets;
+  }
+
+  Widget getCategoryWidget(Category category) {
+    return FlatButton(
+      child: Text(
+        category.categoryName,
+        style: TextStyle(color: Colors.blueGrey),
+      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+          side: BorderSide(color: Colors.lightGreenAccent)),
+    );
+  }
+}
