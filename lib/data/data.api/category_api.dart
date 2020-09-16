4c6df0e14117c4bb8944631e6938db54d79f14de
volkannaktass <volkanaktas98@gmail.com>
2020-09-16 22:33:06 +0300

    first commit

--- lib/data/data.api/category_api.dart
+++ lib/data/data.api/category_api.dart
@@ -2,6 +2,6 @@ import 'package:http/http.dart' as http;
 
 class CategoryApi {
   static Future getCategories() {
-    return http.get("http://10.0.2.2:3000/categories");
+    return http.get("http://127.0.0.1:3000/categories");
   }
 }
